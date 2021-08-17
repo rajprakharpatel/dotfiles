bcf06ff85018273d623314e698be1e7cca5ed1f1
rajprakhar <rajprakhar2000@gmail.com>
2021-08-17 17:47:32 +0530

    updated lfrc to use xdg-open

--- linux/.doom.d/config.el
+++ linux/.doom.d/config.el
@@ -107,14 +107,14 @@
 ;  :config
 ;  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-(use-package! org-roam
-  :ensure t
-  :init
-  (setq org-roam-v2-ack t)
-  :custom
-  (org-roam-directory "~/org/Notes")
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
-  :config
-  (org-roam-setup))
+;(use-package! org-roam
+;  :ensure t
+;  :init
+;  (setq org-roam-v2-ack t)
+;  :custom
+;  (org-roam-directory "~/org/Notes")
+;  :bind (("C-c n l" . org-roam-buffer-toggle)
+;         ("C-c n f" . org-roam-node-find)
+;         ("C-c n i" . org-roam-node-insert))
+;  :config
+;  (org-roam-setup))
